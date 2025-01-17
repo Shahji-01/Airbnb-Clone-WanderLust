66210fe712a564950e8ea525b6f8f76262ee2807
SHAH JI <abhinavshah133@gmail.com>
2025-01-18 01:02:27 +0530

    add new Ui

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-if(process.env.NODE_ENV !="production"){
-require('dotenv').config()
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
-// console.log(process.env.SECRET); 
+// console.log(process.env.SECRET);
 
 //All Requrie's
 const express = require("express");
 const app = express();
 const path = require("path");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ExpressError = require("./util/ExpressError.js");
 const listings_route = require("./routes/listings.js");
 const reviews_route = require("./routes/review.js");
-const users_route=require("./routes/user.js");
+const users_route = require("./routes/user.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const { date } = require("joi");
-const flash=require("connect-flash");
-const passport=require("passport");
-const LocalStrategy=require("passport-local");
-const User=require("./models/users.js");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/users.js");
 const { serialize } = require("v8");
+const list = require("./models/listening.js");
 
+const MongoDb_url = "mongodb://127.0.0.1:27017/WanderLust";
+// const DbUrl=process.env.ATLASDB_URL;
 
-// const MongoDb_url='mongodb://127.0.0.1:27017/WanderLust';
-const DbUrl=process.env.ATLASDB_URL;
+// const store=MongoStore.create({
+//     mongoUrl:DbUrl,
+//     crypto:{
+//          secret: process.env.SECRET,
+//     },
+//     touchAfter:24*3600,
+// });
 
-const store=MongoStore.create({
-    mongoUrl:DbUrl,
-    crypto:{
-         secret: process.env.SECRET,
-    },
-    touchAfter:24*3600,
-});
-
-store.on("error",()=>{
-    console.log("Error in mongostore.",err);
-})
+// store.on("error",()=>{
+//     console.log("Error in mongostore.",err);
+// })
 
 const sessionOption = {
-    store,
-    secret:process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        mazAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
+  // store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    mazAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
-
 
 app.use(session(sessionOption));
 app.use(flash());
@@ -64,8 +63,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 // for views(ejs)
 app.set("view engine", "ejs");
@@ -76,25 +74,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // getting-started.js
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("db connection is done.");
-}).catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect(DbUrl);
+  await mongoose.connect(MongoDb_url);
 }
 
 const port = 8080;
 app.listen(port, () => {
-    console.log(`server is listening to port:${port}.`);
+  console.log(`server is listening to port:${port}.`);
 });
 
-
-app.use((req,res,next)=>{
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-    res.locals.CurrUser=req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.CurrUser = req.user;
+  next();
 });
 
 // demouser
@@ -113,13 +112,12 @@ app.use("/listings/:id/reviews", reviews_route);
 app.use("/", users_route);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found"));
+  next(new ExpressError(404, "Page Not Found"));
 });
 app.use((err, req, res, next) => {
-    let { status = 500, message = "Something Went Wrong" } = err;
-    // res.status(status).send(message);
-    res.status(status).render("err.ejs", { err });
-
+  let { status = 500, message = "Something Went Wrong" } = err;
+  // res.status(status).send(message);
+  res.status(status).render("err.ejs", { err });
 });
 // app.get("/testListening",async (req,res)=>{
 //     let samplelist=list({
--- routes/listings.js
+++ routes/listings.js
@@ -6,26 +6,42 @@ const WrapAsync = require("../util/WrapAsync.js");
 const { listSchema } = require("../schema.js");
 const { IsLoggedIn, IsOwner, validationListing } = require("../middleware.js");
 const listingController = require("../controller/listting.js");
-const multer  = require('multer');
-const {storage}=require("../CloudConfig.js");
+const multer = require("multer");
+const { storage } = require("../CloudConfig.js");
 const upload = multer({ storage });
 
 // Home route
-router.route("/")
-    .get(listingController.index)
-    .post(IsLoggedIn,upload.single('list[image]'),validationListing,WrapAsync(listingController.createListing));
+router
+  .route("/")
+  .get(listingController.index)
+  .post(
+    IsLoggedIn,
+    upload.single("list[image]"),
+    validationListing,
+    WrapAsync(listingController.createListing)
+  );
 // New route
 router.get("/new", IsLoggedIn, listingController.newListing);
 
-router.route("/:id")
-    .get(WrapAsync(listingController.showListing))
-    .patch(IsLoggedIn, IsOwner,upload.single('list[image]'), validationListing, WrapAsync(listingController.upadateListing))
-    .delete(IsLoggedIn, IsOwner, WrapAsync(listingController.deleteListing));
-
-
+router.post("/search", listingController.Search);
+router
+  .route("/:id")
+  .get(WrapAsync(listingController.showListing))
+  .patch(
+    IsLoggedIn,
+    IsOwner,
+    upload.single("list[image]"),
+    validationListing,
+    WrapAsync(listingController.upadateListing)
+  )
+  .delete(IsLoggedIn, IsOwner, WrapAsync(listingController.deleteListing));
 
 // edit route
-router.get("/:id/edit", IsLoggedIn, IsOwner, WrapAsync(listingController.editListing));
-
+router.get(
+  "/:id/edit",
+  IsLoggedIn,
+  IsOwner,
+  WrapAsync(listingController.editListing)
+);
 
 module.exports = router;
